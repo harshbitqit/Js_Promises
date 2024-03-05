c62a1ab83a58aa07500d19cb6376e1d7660d1afb
harshbitqit <harsh@bitqit.com>
2024-03-05 17:58:15 +0530

    Promise

---
+++ Promise.all.js
@@ -0,0 +1,26 @@
+const p1= new Promise ((resolve,reject)=>{
+    setTimeout(()=> resolve("P1 Success"),5000);
+})
+
+const p2 = new Promise((resolve,reject)=>{
+  setTimeout(()=> resolve("P2 Success"),1000);
+//   setTimeout(()=> reject("P2 Fail"),1000);
+})
+
+const p3 = new Promise((resolve,reject)=>{
+    // setTimeout(()=> resolve("P3 Success"),2000);
+    setTimeout(()=> reject("P3 Fail"),2000);
+})
+
+Promise.all([p1,p2,p3]).then(res=>{
+    console.log(res);
+})
+.catch((err)=>{
+    console.error(err);
+});
+
+// Promise.all Takes an array of promises,  THese promises can also be API calls
+// p1,p2,p3 are promises
+
+
+
